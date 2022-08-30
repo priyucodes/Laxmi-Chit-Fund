528f7bd0878db287479d038f8b91650e5a96568b
Priyanshu Bhandari <technopriyu@gmail.com>
2022-08-30 20:35:25 +0530

    Added loading spinner

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import {
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import PropagateLoader from 'react-spinners/PropagateLoader';
 import Header from '../components/Header';
 import Login from '../components/Login';
 
@@ -20,14 +21,25 @@ const Home: NextPage = () => {
 
   if (isLoading) {
     return (
-      <div>
-        <div>
-          
+      <div className="bg-[#091B18] h-screen flex flex-col items-center justify-center">
+        <div className="flex items-center space-x-2 mb-10">
+          <Image
+            className="rounded-full h-20 w-20"
+            height={'100%'}
+            width={'100%'}
+            src="https://i.imgur.com/4h7mAu7.png"
+            alt="hero"
+          />
+          <h1 className="text-lg text-white font-bold">
+            Loading The Laxmi Chit Fund
+          </h1>
         </div>
+        <PropagateLoader color="white" size={30} />
       </div>
-    )
+    );
   }
   if (!address) return <Login />;
+
   return (
     <div className="bg-[#091B18] min-h-screen flex flex-col">
       <Head>
